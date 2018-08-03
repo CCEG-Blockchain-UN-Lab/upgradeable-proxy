c1ec6e8fdb6ac6bd800d68bd9bb7f6356beacf8e
Prophet Daniel <prophetdaniel@ethereumclassic.org>
2018-08-03 17:29:22 -0300

    :bug: Fix contract tests to correctly display in order.

--- test/UintEvent.js
+++ test/UintEvent.js
@@ -1,55 +1,89 @@
-const Proxy = artifacts.require('Proxy')
-const UintEventV1 = artifacts.require('UintEventV1')
-const UintEventV2a_RemovedEvent = artifacts.require('UintEventV2a_RemovedEvent')
-const UintEventV2b_EventReordered = artifacts.require('UintEventV2b_EventReordered')
+const Proxy = artifacts.require("Proxy");
+const UintEventV1 = artifacts.require("UintEventV1");
+const UintEventV2a_RemovedEvent = artifacts.require(
+  "UintEventV2a_RemovedEvent"
+);
+const UintEventV2b_EventReordered = artifacts.require(
+  "UintEventV2b_EventReordered"
+);
 
-const INDENT = '      ';
+const INDENT = "      ";
 
-contract('UintEvent', function (accounts) {
-
-    let proxy,
+contract("UintEvent", function(accounts) {
+  let proxy,
     uintEventV1,
     uintEventV2a_RemovedEvent,
     uintEventV2b_EventReordered,
     uintEventV1byProxy;
 
-    const inputValue = 10, inputValue2 = 21, inputValue3 = 32, inputValue4 = 43;
+  const inputValue = 10,
+    inputValue2 = 21,
+    inputValue3 = 32,
+    inputValue4 = 43;
 
-    beforeEach(async function() {
-        uintEventV1 = await UintEventV1.new();
-        uintEventV2a_RemovedEvent = await UintEventV2a_RemovedEvent.new();
-        uintEventV2b_EventReordered = await UintEventV2b_EventReordered.new();
-        proxy = await Proxy.new(uintEventV1.address);
-        uintEventV1byProxy = UintEventV1.at(proxy.address);
-        await uintEventV1byProxy.initialize();
-    })
+  beforeEach(async function() {
+    uintEventV1 = await UintEventV1.new();
+    uintEventV2a_RemovedEvent = await UintEventV2a_RemovedEvent.new();
+    uintEventV2b_EventReordered = await UintEventV2b_EventReordered.new();
+    proxy = await Proxy.new(uintEventV1.address);
+    uintEventV1byProxy = UintEventV1.at(proxy.address);
+    await uintEventV1byProxy.initialize();
+  });
 
-    it('should upgrade the contract UintEvent to version 2a with event removed', async function () {
-        let tx = await uintEventV1byProxy.setValue(inputValue)
-        let valueChangedLog = tx.logs[0]
-        assert.equal(valueChangedLog.event, "EventValueChanged", "First log should be EventValueChanged")
-        assert.equal(valueChangedLog.args.newValue.toNumber(), inputValue, "The new value should be inputValue")
+  it("should upgrade the contract UintEvent to version 2a with event removed", async function() {
+    let tx = await uintEventV1byProxy.setValue(inputValue);
+    let valueChangedLog = tx.logs[0];
+    assert.equal(
+      valueChangedLog.event,
+      "EventValueChanged",
+      "First log should be EventValueChanged"
+    );
+    assert.equal(
+      valueChangedLog.args.newValue.toNumber(),
+      inputValue,
+      "The new value should be inputValue"
+    );
 
-        await uintEventV1byProxy.upgradeTo(uintEventV2a_RemovedEvent.address)
-        await uintEventV1byProxy.initialize();
+    await uintEventV1byProxy.upgradeTo(uintEventV2a_RemovedEvent.address);
+    await uintEventV1byProxy.initialize();
 
-        tx = await uintEventV1byProxy.setValue(inputValue)
-        valueChangedLog = tx.logs[0]
-        assert.strictEqual(valueChangedLog, undefined, 'There should be no event emitted')
-    })
+    tx = await uintEventV1byProxy.setValue(inputValue);
+    valueChangedLog = tx.logs[0];
+    assert.strictEqual(
+      valueChangedLog,
+      undefined,
+      "There should be no event emitted"
+    );
+  });
 
-    it('should upgrade the contract UintEvent to version 2b with events in reverse order', async function () {
-        let tx = await uintEventV1byProxy.setValue(inputValue)
-        let valueChangedLog = tx.logs[0]
-        assert.equal(valueChangedLog.event, "EventValueChanged", "First log should be EventValueChanged")
-        assert.equal(valueChangedLog.args.newValue.toNumber(), inputValue, "The new value should be inputValue")
+  it("should upgrade the contract UintEvent to version 2b with events in reverse order", async function() {
+    let tx = await uintEventV1byProxy.setValue(inputValue);
+    let valueChangedLog = tx.logs[0];
+    assert.equal(
+      valueChangedLog.event,
+      "EventValueChanged",
+      "First log should be EventValueChanged"
+    );
+    assert.equal(
+      valueChangedLog.args.newValue.toNumber(),
+      inputValue,
+      "The new value should be inputValue"
+    );
 
-        await uintEventV1byProxy.upgradeTo(uintEventV2b_EventReordered.address)
-        await uintEventV1byProxy.initialize();
-        
-        tx = await uintEventV1byProxy.setValue(inputValue)
-        valueChangedLog = tx.logs[0]
-        assert.equal(valueChangedLog.event, "EventValueChanged", "First log should be EventValueChanged")
-        assert.equal(valueChangedLog.args.newValue.toNumber(), inputValue, "The new value should be inputValue")
-    })
-})
+    await uintEventV1byProxy.upgradeTo(uintEventV2b_EventReordered.address);
+    await uintEventV1byProxy.initialize();
+
+    tx = await uintEventV1byProxy.setValue(inputValue);
+    valueChangedLog = tx.logs[0];
+    assert.equal(
+      valueChangedLog.event,
+      "EventValueChanged",
+      "First log should be EventValueChanged"
+    );
+    assert.equal(
+      valueChangedLog.args.newValue.toNumber(),
+      inputValue,
+      "The new value should be inputValue"
+    );
+  });
+});
--- test/UpgradeCheck.js
+++ test/UpgradeCheck.js
@@ -1,104 +1,133 @@
 const CheckContract = artifacts.require("CheckContract");
-const Proxy = artifacts.require("Proxy");
-const SafeProxy = artifacts.require('SafeProxy')
-const UpgradeCheck_CanUpgrade = artifacts.require('UpgradeCheck_CanUpgrade')
-const UpgradeCheck_CannotUpgrade = artifacts.require('UpgradeCheck_CannotUpgrade')
-const UpgradeCheckV2_CanUpgrade = artifacts.require('UpgradeCheckV2_CanUpgrade')
-const UpgradeCheckV2b_CannotUpgrade = artifacts.require('UpgradeCheckV2b_CannotUpgrade')
-const UpgradeCheckV3_CanUpgrade = artifacts.require('UpgradeCheckV3_CanUpgrade')
+const SafeProxy = artifacts.require("SafeProxy");
+const UpgradeCheck_CanUpgrade = artifacts.require("UpgradeCheck_CanUpgrade");
+const UpgradeCheck_CannotUpgrade = artifacts.require(
+  "UpgradeCheck_CannotUpgrade"
+);
+const UpgradeCheckV2_CanUpgrade = artifacts.require(
+  "UpgradeCheckV2_CanUpgrade"
+);
+const UpgradeCheckV2b_CannotUpgrade = artifacts.require(
+  "UpgradeCheckV2b_CannotUpgrade"
+);
+const UpgradeCheckV3_CanUpgrade = artifacts.require(
+  "UpgradeCheckV3_CanUpgrade"
+);
 
-const INDENT = '      ';
+const INDENT = "      ";
 
-contract('UpgradeCheck', function (accounts) {
-
-    let safeProxy,
+contract("UpgradeCheck", function(accounts) {
+  let safeProxy,
     upgradeCheck_CanUpgrade,
     upgradeCheck_CannotUpgrade,
     upgradeCheckV2_CanUpgrade,
     upgradeCheckV2a_CannotUpgrade,
     upgradeCheckV2b_CannotUpgrade,
     upgradeCheckV3_CanUpgrade,
-    upgradeCheckbySafeProxy;
+    upgradeCheckbySafeProxy,
+    checkContractAddress;
 
-    const inputValue = 10;
+  const inputValue = 10;
 
-    CheckContract.deployed().then(checkContractInstance => {
-      beforeEach(async function() {
-        // let checkContractInstance = await CheckContract.new();
-        // let proxyInstance = await Proxy.new(checkContractInstance.address);
-        // let checkContractInstanceByProxy = CheckContract.at(proxyInstance.address)
-        // await checkContractInstanceByProxy.initialize();
-        let result = await Promise.all([
-          UpgradeCheck_CanUpgrade.new(),
-          UpgradeCheck_CannotUpgrade.new(),
-          UpgradeCheckV2_CanUpgrade.new(),
-          UpgradeCheckV2b_CannotUpgrade.new(),
-          UpgradeCheckV3_CanUpgrade.new()
-        ])
-        upgradeCheck_CanUpgrade = result[0];
-        upgradeCheck_CannotUpgrade = result[1];
-        upgradeCheckV2_CanUpgrade = result[2];
-        upgradeCheckV2b_CannotUpgrade = result[3];
-        upgradeCheckV3_CanUpgrade = result[4];
+  beforeEach(async function() {
+    let result = await Promise.all([
+      UpgradeCheck_CanUpgrade.new(),
+      UpgradeCheck_CannotUpgrade.new(),
+      UpgradeCheckV2_CanUpgrade.new(),
+      UpgradeCheckV2b_CannotUpgrade.new(),
+      UpgradeCheckV3_CanUpgrade.new()
+    ]);
+    upgradeCheck_CanUpgrade = result[0];
+    upgradeCheck_CannotUpgrade = result[1];
+    upgradeCheckV2_CanUpgrade = result[2];
+    upgradeCheckV2b_CannotUpgrade = result[3];
+    upgradeCheckV3_CanUpgrade = result[4];
 
-        safeProxy = await SafeProxy.new(upgradeCheck_CanUpgrade.address, checkContractInstance.address);
-        upgradeCheckbySafeProxy = UpgradeCheck_CanUpgrade.at(safeProxy.address);
-        await upgradeCheckbySafeProxy.initialize();
-      })
+    checkContractAddress = (await CheckContract.deployed()).address;
 
-      it('should be able to deploy SafeProxy with upgradeable contract target', async function () {
-        await SafeProxy.new(upgradeCheck_CanUpgrade.address, checkContractInstance.address);
-      })
+    safeProxy = await SafeProxy.new(
+      upgradeCheck_CanUpgrade.address,
+      checkContractAddress
+    );
+    upgradeCheckbySafeProxy = UpgradeCheck_CanUpgrade.at(safeProxy.address);
+    await upgradeCheckbySafeProxy.initialize();
+  });
 
-      it('should upgrade to an upgradeable contract', async function () {
-        await safeProxy.upgradeTo(upgradeCheckV2_CanUpgrade.address)
-      })
+  it("should be able to deploy SafeProxy with upgradeable contract target", async function() {
+    await SafeProxy.new(upgradeCheck_CanUpgrade.address, checkContractAddress);
+  });
 
-      it('should not be able to upgrade to itself', async function () {
-        try {
-          await safeProxy.upgradeTo(upgradeCheck_CanUpgrade.address)
-          throw new Error("This error should not happen")
-        } catch (error) {
-          assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Cannot upgrade to self")
-        }
-      })
+  it("should upgrade to an upgradeable contract", async function() {
+    await safeProxy.upgradeTo(upgradeCheckV2_CanUpgrade.address);
+  });
 
-      it('should not be able to deploy SafeProxy with non-upgradeable contract', async function () {
-        try {
-          await SafeProxy.new(upgradeCheck_CannotUpgrade.address, checkContractInstance.address);
-          throw new Error("This error should not happen")
-        } catch (error) {
-          assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Cannot deploy SafeProxy to a non-upgradeable contract")
-        }
-      })
+  it("should not be able to upgrade to itself", async function() {
+    try {
+      await safeProxy.upgradeTo(upgradeCheck_CanUpgrade.address);
+      throw new Error("This error should not happen");
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: invalid opcode",
+        "Cannot upgrade to self"
+      );
+    }
+  });
 
-      it('should not be able to upgrade to a non-upgradeable contract', async function () {
-        try {
-          await safeProxy.upgradeTo(upgradeCheck_CannotUpgrade.address)
-          throw new Error("This error should not happen")
-        } catch (error) {
-          assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Cannot upgrade to a non-upgradeable contract")
-        }
-      })
+  it("should not be able to deploy SafeProxy with non-upgradeable contract", async function() {
+    try {
+      await SafeProxy.new(
+        upgradeCheck_CannotUpgrade.address,
+        checkContractAddress
+      );
+      throw new Error("This error should not happen");
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: invalid opcode",
+        "Cannot deploy SafeProxy to a non-upgradeable contract"
+      );
+    }
+  });
 
-      it('should not be able to upgrade to a non-contract', async function () {
-        try {
-          await SafeProxy.new(accounts[1], checkContractInstance.address);
-          throw new Error("This error should not happen")
-        } catch (error) {
-          assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Cannot upgrade to a non-contract")
-        }
-      })
+  it("should not be able to upgrade to a non-upgradeable contract", async function() {
+    try {
+      await safeProxy.upgradeTo(upgradeCheck_CannotUpgrade.address);
+      throw new Error("This error should not happen");
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: invalid opcode",
+        "Cannot upgrade to a non-upgradeable contract"
+      );
+    }
+  });
 
-      it('should only upgrade if upgradeTo(address) functions exist', async function() {
-        try {
-          await safeProxy.upgradeTo(upgradeCheckV2b_CannotUpgrade.address)
-          throw new Error("This error should not happen")
-        } catch (error) {
-          assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Cannot upgrade to a non-contract")
-        }
+  it("should not be able to upgrade to a non-contract", async function() {
+    try {
+      await SafeProxy.new(accounts[1], checkContractAddress);
+      throw new Error("This error should not happen");
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: invalid opcode",
+        "Cannot upgrade to a non-contract"
+      );
+    }
+  });
 
-        await safeProxy.upgradeTo(upgradeCheckV3_CanUpgrade.address)
-      })
-    })
-})
+  it("should only upgrade if upgradeTo(address) functions exist", async function() {
+    try {
+      await safeProxy.upgradeTo(upgradeCheckV2b_CannotUpgrade.address);
+      throw new Error("This error should not happen");
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "VM Exception while processing transaction: invalid opcode",
+        "Cannot upgrade to a non-contract"
+      );
+    }
+
+    await safeProxy.upgradeTo(upgradeCheckV3_CanUpgrade.address);
+  });
+});
