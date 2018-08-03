bf0cc94a394e6d553a44dd389d35f1b6a26ae293
Prophet Daniel <prophetdaniel@ethereumclassic.org>
2018-08-03 16:44:33 -0300

    :art: Restructure migration's code.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,19 +7,16 @@ const assert = require('assert');
  * https://github.com/trufflesuite/truffle/issues/501
  */
 
-// The async function
-const theAsyncFunction = async function theAsyncFunction(deployer) {
-  let checkContractInstance = await deployer.deploy(CheckContract);
-  let proxyInstance = await deployer.deploy(Proxy, checkContractInstance.address);
-  let checkContractInstanceByProxy = CheckContract.at(proxyInstance.address);
-  await checkContractInstanceByProxy.initialize();
-  let testOne = await checkContractInstanceByProxy.isContract.call(checkContractInstanceByProxy.address);
-  assert.equal(testOne, true);
-  let testTwo = await checkContractInstanceByProxy.isContract.call('0x627306090abab3a6e1400e9345bc60c78a8bef57');
-  assert.equal(testTwo, false);
-};
-
-module.exports = function (deployer) {
+module.exports = (deployer) => {
   // Alternatively, just start a chain without a deployment
-  deployer.then(async () => await theAsyncFunction(deployer));
+  deployer.then(async () => {
+    let checkContractInstance = await deployer.deploy(CheckContract);
+    let proxyInstance = await deployer.deploy(Proxy, checkContractInstance.address);
+    let checkContractInstanceByProxy = CheckContract.at(proxyInstance.address);
+    await checkContractInstanceByProxy.initialize();
+    let testOne = await checkContractInstanceByProxy.isContract.call(checkContractInstanceByProxy.address);
+    assert.equal(testOne, true);
+    let testTwo = await checkContractInstanceByProxy.isContract.call('0x627306090abab3a6e1400e9345bc60c78a8bef57');
+    assert.equal(testTwo, false);
+  });
 };
